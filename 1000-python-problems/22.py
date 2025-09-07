b82dcd194863687ec6a30c8dc41c35252d8e0892
Kasra Namiranian <114209244+KasraNamiranian@users.noreply.github.com>
2025-09-07 22:21:54 +0330

    Add files via upload

---
+++ 1000-python-problems/22.py
@@ -0,0 +1,3 @@
+salary = int(input("Please enter your salary: "))
+reward = salary * 0.15
+print("The reward is: ",reward)
---
+++ 1000-python-problems/25.py
@@ -0,0 +1,6 @@
+# good question
+k = int(input("Enter k: "))
+x = int(input("Enter x: "))
+n = int(input("Enter n: "))
+acceleration = (k-x)*60/n
+print("acceleration is ",acceleration)
