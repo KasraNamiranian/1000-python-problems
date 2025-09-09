6eb68bec65739efb8e5154aba06a66f81fd7cb5e
Kasra Namiranian <114209244+KasraNamiranian@users.noreply.github.com>
2025-09-10 00:02:01 +0330

    Add files via upload

---
+++ 1000-python-problems/35.py
@@ -0,0 +1,4 @@
+liter = float(input("Please enter the fuel consumption of your can in liter: "))
+mile = float(input("Please enter the distance that your car went through: "))
+result = mile/liter * 0.264179
+print("miles / galon is ", result)
